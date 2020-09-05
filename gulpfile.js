a8a6f910c04a82baa3fe7011e885cd010d66d563
Hideki Yamamura <hideki.yamamura@gmail.com>
2020-09-05 18:29:53 +0900

    rename variable

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@ const gulp = require('gulp');
 const del = require('del');
 const hb = require('gulp-hb');
 
-const dist = 'dist';
+const dest = 'dist';
 
 gulp.task('clean', () => {
-    return del(dist);
+    return del(dest);
 });
 
 gulp.task('handlebars', () => {
     return gulp.src('src/templates/**/*.html')
         .pipe(hb().partials('src/partials/**/*.hbs'))
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('static', () => {
     return gulp.src('src/static/**/*')
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('build', gulp.parallel('handlebars', 'static'));
