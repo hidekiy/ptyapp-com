f3500c579c8c657e3c3af842f0b5a057997ed3e3
Hideki Yamamura <hideki.yamamura@gmail.com>
2020-09-05 17:00:35 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,23 @@ const gulp = require('gulp');
 const del = require('del');
 const hb = require('gulp-hb');
 
+const dist = 'dist';
+
 gulp.task('clean', () => {
-    return del(['dist']);
+    return del(dist);
 });
 
-gulp.task('handlebars', gulp.series('clean', () => {
+gulp.task('handlebars', () => {
     return gulp.src('src/templates/**/*.html')
         .pipe(hb().partials('src/partials/**/*.hbs'))
-        .pipe(gulp.dest('dist'));
-}));
+        .pipe(gulp.dest(dist));
+});
 
-gulp.task('static', gulp.series('clean', () => {
+gulp.task('static', () => {
     return gulp.src('src/static/**/*')
-        .pipe(gulp.dest('dist'));
-}));
+        .pipe(gulp.dest(dist));
+});
 
 gulp.task('build', gulp.parallel('handlebars', 'static'));
 
-gulp.task('default', gulp.task('build'));
+gulp.task('default', gulp.series('clean', 'build'));
